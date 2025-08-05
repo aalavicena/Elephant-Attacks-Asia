76b3679bd03ffd8c0df19a2b72b64f745a0d5005
aalavicena <92833717+aalavicena@users.noreply.github.com>
2025-08-05 17:07:46 +0800

    Update and rename 7-cross validation.R to 5-cross validation.R

--- 5-cross validation.R
+++ 5-cross validation.R
@@ -1,9 +1,7 @@
 library(sperrorest)
 
+# Custom predict function 
 predict_spaMM <- function(object, newdata, ...) {
-  if (!requireNamespace("spaMM", quietly = TRUE)) {
-    stop("Package 'spaMM' is required for this function.")
-  }
   preds <- spaMM::predict(
     object,
     newdata = newdata,
@@ -13,20 +11,13 @@ predict_spaMM <- function(object, newdata, ...) {
   return(as.vector(preds))
 }
 
+# Formula
 model_formula <- att ~ ed + mesh + tri + wetness + hfp + popdens + gdp + dist_forest + dist_cropland + dist_fp + dist_pa + I(wetness^2) + I(dist_forest^2) + I(dist_cropland^2) + I(dist_fp^2) + I(dist_pa^2) + Matern(1|y+x)
 
-predict_spaMM <- function(object, newdata, ...) {
-  preds <- predict(
-    object,
-    newdata = newdata,
-    re.form = NA, 
-    type = "response"
-  )
-  return(as.vector(preds))
-}
-
+# Parallel processing
 nthr <- parallel::detectCores(logical = FALSE) - 1L
 
+# Cross-validation
 sp_cv <- sperrorest(
   data = final.data,
   formula = model_formula,
